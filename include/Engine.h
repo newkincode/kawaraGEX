539c608129de127a7aa8378c6ec28b2a1e10a564
the-real-ddpurple <jiany0709@naver.com>
2025-08-11 19:57:02 -0700

    대충

---
+++ include/Engine.h
@@ -0,0 +1,9 @@
+#ifndef KAWARAGEX_ENGINE_H
+#define KAWARAGEX_ENGINE_H
+
+
+class Engine {
+};
+
+
+#endif //KAWARAGEX_ENGINE_H
---
+++ src/core/Engine.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jiany on 8/11/2025.
+//
+
+#include "Engine.h"
