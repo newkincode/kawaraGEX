c7c3a75e63d36999e8ffbd705c57505764cabe8a
newkincode <maainnewkin59@gmail.com>
2025-08-31 02:33:38 +0900

    edit GEX_Engine.h

--- include/GEX/GEX_Engine.h
+++ include/GEX/GEX_Engine.h
@@ -1,17 +1,25 @@
 #ifndef GEX_ENGINE_H
 #define GEX_ENGINE_H
 
+#include <list>
 #include <string>
+
+#include "GEX_Scene.h"
 
 class Engine {
 private:
   std::string title;
+
+  std::list<GEX::Scene*> scenes;
 
 public:
   Engine(); // Untitled 생성자
   Engine(const std::string& t); // Title 을 받는 생성자
   std::string getTitle() const;
   void setTitle(const std::string& t);
+  void registScene(GEX::Scene* scene);
+  void unregistScene(GEX::Scene* scene);
+
 };
 
 #endif // GEX_ENGINE_H
