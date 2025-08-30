fa8bd629e2c2b89fe8f28703772deb8539f3a7f6
newkincode <maainnewkin59@gmail.com>
2025-08-31 01:47:30 +0900

    Engine

--- include/GEX/GEX_Engine.h
+++ include/GEX/GEX_Engine.h
@@ -1,9 +1,17 @@
-#ifndef KAWARAGEX_ENGINE_H
-#define KAWARAGEX_ENGINE_H
+#ifndef GEX_ENGINE_H
+#define GEX_ENGINE_H
 
-namespace GEX {
-  class Engine {
-  };
-}
+#include <string>
 
-#endif //KAWARAGEX_ENGINE_H
+class Engine {
+private:
+  std::string title;
+
+public:
+  Engine(); // Untitled 생성자
+  Engine(const std::string& t); // Title 을 받는 생성자
+  std::string getTitle() const;
+  void setTitle(const std::string& t);
+};
+
+#endif // GEX_ENGINE_H
--- src/core/Engine.cpp
+++ src/core/Engine.cpp
@@ -1,1 +1,13 @@
 #include "GEX_Engine.h"
+
+Engine::Engine() : title("Untitled") {}
+
+Engine::Engine(const std::string& t) : title(t) {}
+
+std::string Engine::getTitle() const {
+    return title;
+}
+
+void Engine::setTitle(const std::string& t) {
+    title = t;
+}
